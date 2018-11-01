33f44b91b6550b40416033a60b3f9d07214f1e6c
Osmar Pereira da Silva Junior <bibliotecaosmar@gmail.com>
2018-11-01 15:26:33 -0300

    Some works in language class.

--- app/http/controllers/Language.php
+++ app/http/controllers/Language.php
@@ -14,39 +14,17 @@
         protected $guide;
         protected $ourproposal;
 
-        private function setVariables($index, 
-                                      $plant, 
-                                      $animal, 
-                                      $insect, 
-                                      $mushroom, 
-                                      $guide, 
-                                      $ourproposal){
-            $this->index = $index;
-            $this->plant = $plant;
-            $this->animal = $animal;
-            $this->insect = $insect;
-            $this->mushroom = $mushroom;
-            $this->guide = $guide;
-            $this->ourproposal = $ourproposal;
-        }
         protected function setLanguage($language){
-            switch($language){
-                case 'portuguese':
-                    $this->portuguese();
-                case 'english':
-                    $this->english();
-                default:
-                    $this->portuguese();
+            $test = $language;
+            if($test = 'pt-br'||'en'){
+                require_once ROOT . VIEW . LANG . $language . '.php';
+                $this->index = $index;
+                $this->plant = $plant;
+                $this->animal = $animal;
+                $this->insect = $insect;
+                $this->mushroom = $mushroom;
+                $this->guide = $guide;
+                $this->ourproposal = $ourproposal;
             }
-        }
-
-        private function portuguese(){
-            require_once ROOT . VIEW . LANG . 'pt-br.php';
-            $this->setVariables($index, $plant, $animal, $insect, $mushroom, $guide, $ourproposal);
-        }
-
-        private function english(){
-            require_once ROOT . VIEW . LANG . 'en.php';
-            $this->setVariables($index, $plant, $animal, $insect, $mushroom, $guide, $ourproposal);
         }
     }
--- resource/lang/require_lang.php
+++ resource/lang/require_lang.php
@@ -1,7 +1,7 @@
 <h3>Choice</h3>
     <form action="index.php" method="post">
-        <input type="radio" name="setLanguage" value="english" method="post"><label for="english">English</label>
-        <input type="radio" name="setLanguage" value="portuguese" method="post"><label for="portuguese">Português</label>
+        <input type="radio" name="setLanguage" value="en" method="post"><label for="english">English</label>
+        <input type="radio" name="setLanguage" value="pt-br" method="post"><label for="portuguese">Português</label>
         <input type="radio" name="setLanguage" value="newly" method="post"><label for="newly">Newly</label>
         <input type="submit" value="confirm">
     </form>
