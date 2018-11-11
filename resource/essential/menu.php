52ec59a06f6921a5a8d39b89478b2819203d34f8
Osmar Pereira da Silva Junior <bibliotecaosmar@gmail.com>
2018-11-10 23:42:27 -0300

    Add  elementies for menu.

--- resource/essential/menu.php
+++ resource/essential/menu.php
@@ -1,3 +1,7 @@
+                    <form method="post">
+                        <input id="Guide" type="submit" hidden="value" name="view" value="guide"/><label for="Guide"><?php echo $this->guide ?></label>
+                        <input id="Ourproposal" type="submit" hidden="value" name="view" value="ourproposal"/><label for="Ourproposal"><?php echo $this->ourproposal ?></label>
+                    </form><br>
                     <form id="tabs" method="post">
                         <input id="Index" type="submit" hidden="value" name="view" value="index"/><label for="Index"><?php echo $this->index ?></label>
                         <input id="Plant" type="submit" hidden="value" name="view" value="plant"/><label for="Plant"><?php echo $this->plant ?></label>
